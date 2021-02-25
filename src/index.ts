f1063398904c48b6431da8af7c0b8ca3e7025097
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-02-25 16:44:02 +0900

    chore(example): update example

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,29 @@
 //    - values の場合は Select タグとする
 //    - プロパティの追加は可能だが、削除は不可とする
 
-const data = [
+const defaultData = [
   {
+    elementType: "inputText",
     title: "氏名",
     value: "input value",
   },
   {
+    elementType: "inputText",
     title: "メールアドレス",
     value: `This is article.`,
   },
   {
+    elementType: "inputText",
     title: "電話番号",
-    value: "080-1234-5678",
+    value: `This is article.`,
   },
   {
+    elementType: "select",
     title: "問合せの種類",
     values: ["返品について", "発送について", "その他"],
   },
   {
+    elementType: "textarea",
     title: "問合せ内容",
     value: `This is article.`,
   },
